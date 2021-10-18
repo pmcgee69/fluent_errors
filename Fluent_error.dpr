04060a004e61321a7673b2e3bdd0e2ee01d7be45
paul.mcgee.8@bigpond.com <Paul>
2021-10-19 02:29:03 +0800

    Experiment with array of vmi's

--- Fluent_error.dpr
+++ Fluent_error.dpr
@@ -4,10 +4,12 @@ program Fluent_error;
 uses
   System.SysUtils,   rtti, U_StringTransform in 'U_StringTransform.pas', U_Maybe in 'U_Maybe.pas', U_Common in 'U_Common.pas';
 
-const     arr_size = 1000000;
+const     arr_size = 100000;
 
 type      arrT = array [1..arr_size] of Transformstring;
           arrI = array [1..arr_size] of TMaybe<integer>;
+
+          Tvmi = array [1..arr_size] of TVirtualMethodInterceptor;
 
 var       arr  : arrT;
           arr2 : arrI;
@@ -30,7 +32,7 @@ begin
    quickcheck(arr, arr2);   writeln('-');   cleararray(arr, arr2, arr_size);
    quickcheck(arr, arr2);
 
-   var vmi := TVirtualMethodInterceptor.Create(Transformstring);
+   //var vmi := TVirtualMethodInterceptor.Create(Transformstring);
    (*
    vmi.OnBefore := procedure(  Instance  : TObject;        Method: TRttiMethod;
                                const Args: TArray<TValue>; out DoInvoke: Boolean;  out Result: TValue     )
@@ -40,15 +42,26 @@ begin
                               write(' err  ');
                    }  end;
    *)
+
+   var vmi : Tvmi;
    Timer( procedure
           begin  for var i := 1 to arr_size do  begin
-                         vmi.Proxify( arr[i] );
+                     vmi[i] := TVirtualMethodInterceptor.Create(Transformstring);
+                     //vmi[i].Proxify( arr[i] )
+                     end;
+          end );
+   writeln('prepped');
+
+   Timer( procedure
+          begin  for var i := 1 to arr_size do  begin
+                         vmi[i].Proxify( arr[i] );
                          arr2[i] := arr[i].trunc(4).hextoint.int;
                  end;
           end );
-   vmi.Free;
+   //vmi.Free;
    quickcheck(arr, arr2);   writeln('-');
-
+readln;
+   Timer( procedure begin  for var i := 1 to arr_size do  vmi[i].free;    end );
    Timer( procedure begin  for var i := 1 to arr_size do  arr[i].Free;    end );
    writeln('done.');
    readln;
