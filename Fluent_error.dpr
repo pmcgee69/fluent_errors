1eb238c75d01d05207274c2a6e0e0f52111642b7
paul.mcgee.8@bigpond.com <Paul>
2021-10-19 01:39:34 +0800

    Proxify required per change of Instance.  x20 slowdown.

--- Fluent_error.dpr
+++ Fluent_error.dpr
@@ -4,14 +4,14 @@ program Fluent_error;
 uses
   System.SysUtils,   rtti, U_StringTransform in 'U_StringTransform.pas', U_Maybe in 'U_Maybe.pas', U_Common in 'U_Common.pas';
 
-const     arr_size = 1000;
+const     arr_size = 1000000;
 
 type      arrT = array [1..arr_size] of Transformstring;
           arrI = array [1..arr_size] of TMaybe<integer>;
 
 var       arr  : arrT;
           arr2 : arrI;
-          X    : Transformstring;
+
 begin
    //ReportMemoryLeaksOnShutdown := true;
    for var i := 1 to arr_size do  arr [i] := Transformstring.Create;
@@ -31,29 +31,22 @@ begin
    quickcheck(arr, arr2);
 
    var vmi := TVirtualMethodInterceptor.Create(Transformstring);
-   X := Transformstring.Create;
-   vmi.Proxify( X );
-
-   (**)
+   (*
    vmi.OnBefore := procedure(  Instance  : TObject;        Method: TRttiMethod;
                                const Args: TArray<TValue>; out DoInvoke: Boolean;  out Result: TValue     )
                       begin
-                        Write('[before] ', Method.Name,'  ');
-                        //if Transformstring(Instance).str.err then
-                              //write(' err  ');
-                      end;
-   (**)
+                        //Write('[before] ', Method.Name,'  ');
+                        {if Transformstring(Instance).str.err then
+                              write(' err  ');
+                   }  end;
+   *)
    Timer( procedure
           begin  for var i := 1 to arr_size do  begin
-                         //X := arr[i];
-                         //arr2[i] := X.trunc(4).hextoint.int;
-   var vmi := TVirtualMethodInterceptor.Create(Transformstring);
                          vmi.Proxify( arr[i] );
                          arr2[i] := arr[i].trunc(4).hextoint.int;
-   vmi.Free;
                  end;
           end );
-   //vmi.Free;
+   vmi.Free;
    quickcheck(arr, arr2);   writeln('-');
 
    Timer( procedure begin  for var i := 1 to arr_size do  arr[i].Free;    end );
